c01346d0d72e68083c8d11ed6ca9769072984ce7
Swerobo <swerobo@gmail.com>
2021-03-18 23:15:07 +0100

    Added Sqlite DB

---
+++ Controllers/UserController.cs
@@ -0,0 +1,66 @@
+using System;
+using barista.api.Models;
+using barista.api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace barista.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+public class UserController : ControllerBase
+    {
+        private UserContext _userContext;
+        public UserController(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        // GET api/user
+        [HttpGet("")]
+        public ActionResult<List<User>> Getstrings()
+        {
+            return _userContext.Users.ToList();
+        }
+
+        // GET api/user/5
+        [HttpGet("{id}")]
+        public ActionResult<User> GetstringById(int id)
+        {
+            return _userContext.Users.FirstOrDefault(user => user.Id == id);
+        }
+
+        // POST api/user
+        [HttpPost("")]
+        public ActionResult<User> Poststring(User user)
+        {
+            _userContext.Users.Add(user);
+            _userContext.SaveChanges();
+            return user;
+        }
+
+        // PUT api/user/5
+        [HttpPut("{id}")]
+        public ActionResult<User> Putstring(int id, User user)
+        {
+            User newUser = _userContext.Users.FirstOrDefault(user => user.Id == id);
+            newUser.Name = user.Name;
+            newUser.Email = user.Email;
+            newUser.Password = user.Password;
+            _userContext.SaveChanges();
+            return newUser;
+
+        }
+
+        // DELETE api/user/5
+        [HttpDelete("{id}")]
+        public void DeletestringById(int id)
+        {
+            User oldUser = _userContext.Users.FirstOrDefault(user => user.Id == id);
+            _userContext.Users.Remove(oldUser);
+            _userContext.SaveChanges();
+        }
+    }
+}
